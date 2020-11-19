d1e4b6fbef850ddd1d42f6690d563eda2e75c57d
Katia Bulekova <ktrn@bu.edu>
2020-11-18 20:25:17 -0500

    Add year and day output to printTime python script
    Create a struct_time object that can be used to output current year,
    and time
    
    https://www.programiz.com/python-programming/time
    
    module load python3/3.7.7
    python timePrint.py

--- printTime.py
+++ printTime.py
@@ -1,9 +1,25 @@
+"""
+A simple script to output current time
 
+epoch time
+current local time
+current year
+current day
+"""
 
 
 import time
+
+#epoch time
 seconds = time.time()
 print("Seconds since epoch =", seconds)
 
+#local time
 local_time = time.ctime(seconds)
 print("Local time:", local_time)
+
+
+#time struct
+result = time.gmtime(seconds)
+print("year", result.tm_year)
+print("day", result.tm_mday)
